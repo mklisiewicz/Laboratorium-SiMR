3c0e609b5b10ce9d03c190608f0747a23b746833
Mateusz Klisiewicz <129537461+mklisiewicz@users.noreply.github.com>
2024-06-06 22:43:24 +0200

    Delete build-test-Desktop_Qt_6_7_1_MSVC2019_64bit-Debug directory

--- build-test-Desktop_Qt_6_7_1_MSVC2019_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_test_FILE C:/Users/mmate/SiMR/Laboratorium-SiMR/build-test-Desktop_Qt_6_7_1_MSVC2019_64bit-Debug/test.exe)
-set(__QT_DEPLOY_TARGET_test_RUNTIME_DLLS C:/Qt/6.7.1/msvc2019_64/bin/Qt6Widgetsd.dll;C:/Qt/6.7.1/msvc2019_64/bin/Qt6Guid.dll;C:/Qt/6.7.1/msvc2019_64/bin/Qt6Cored.dll)
